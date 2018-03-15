f196b605ec3996b67b2b3e311b24a0cc038249bd
Gabriele Fontana <gabriele.fontana@mondora.com>
2018-03-15 03:59:06 +0100

    back on html

--- src/server.js
+++ src/server.js
@@ -18,8 +18,8 @@ express()
     .use(express.static('assets'))
     .post('/api/messages', connectorListener)
     .get('/', (req, res) => {
-        const html = "<iframe src='https://webchat.botframework.com/embed/ts-mysupport-chatbot?s=MjIHNBVsg2s.cwA.yMM.CflNdwIK6BS5S9lQGO_vIMcrnLa4dWoglQG9R5Bcnw8'></iframe>";
-        res.send(`<h2><i>Team System</i> hacknight</h2>  <h5>v:${version}</h5>${html}`);
+        //const html = "<iframe src='https://webchat.botframework.com/embed/ts-mysupport-chatbot?s=MjIHNBVsg2s.cwA.yMM.CflNdwIK6BS5S9lQGO_vIMcrnLa4dWoglQG9R5Bcnw8'></iframe>";
+        res.send(`<h2><i>Team System</i> hacknight</h2>  <h5>v:${version}</h5>`);
     })
     .listen(PORT, () => {
         console.log(`TS Hacknight MONDORA Bot Server listening on ${HOSTNAME}:${PORT}`);
