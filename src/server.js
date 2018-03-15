b97b754e4bc8fb6f44fde090e71bf378bf3b2c77
Gabriele Fontana <gabriele.fontana@mondora.com>
2018-03-15 03:29:35 +0100

    aggiunta web chat

--- src/server.js
+++ src/server.js
@@ -18,7 +18,8 @@ express()
     .use(express.static('assets'))
     .post('/api/messages', connectorListener)
     .get('/', (req, res) => {
-        res.send(`<h2><i>Team System</i> hacknight</h2>  <h5>v:${version}</h5>`);
+        const html = "<iframe src='https://webchat.botframework.com/embed/ts-mysupport-chatbot?s=MjIHNBVsg2s.cwA.yMM.CflNdwIK6BS5S9lQGO_vIMcrnLa4dWoglQG9R5Bcnw8'></iframe>";
+        res.send(`<h2><i>Team System</i> hacknight</h2>  <h5>v:${version}</h5>${html}`);
     })
     .listen(PORT, () => {
         console.log(`TS Hacknight MONDORA Bot Server listening on ${HOSTNAME}:${PORT}`);
